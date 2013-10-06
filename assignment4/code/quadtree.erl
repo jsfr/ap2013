05a3f79503899e493c9f994274ef536ce6f3fe68
jsfr <jfr.dev@gmail.com>
2013-10-06 13:22:22 +0200

    Quadtree implemented

--- assignment4/code/quadtree.erl
+++ assignment4/code/quadtree.erl
@@ -5,76 +5,65 @@
 %%% Updated : Aug 2013
 %%% Usage   : Assignemnt for Advanced Programming 2013
 %%%---------------------------------------------------------------------
-%%% Student : [ADD HERE]
-%%% KU-ID   : [ADD HERE]
-%%%---------------------------------------------------------------------
-%%% Student : [ADD HERE]
-%%% KU-ID   : [ADD HERE]
+%%% Student : Jens Fredskov
+%%% KU-ID   : chw752
 %%%---------------------------------------------------------------------
 
 -module(quadtree).
 
--export([start/2, stop/1, addElement/3]).%, mapFunction/3, mapTreeFunction/2]).
+-export([addElement/3, start/2, stop/1, mapFunction/3, mapTreeFunction/2]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Interface
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Starts a quadtree with 
+% Starts a quadtree with
 %   Bound: Eucledean size of tree
 %   Limit: Maximum elements in each leaf
 start(Bound, Limit) ->
-	{ok, spawn(fun() -> quadtreeCoordinatorInit(Bound, Limit) end)}.
+    {ok, spawn(fun () -> quadtreeCoordinatorInit(Bound, Limit) end)}.
 
 stop(Qtree) ->
 	send_stop(Qtree).
 
-% Adds and element 
+% Adds and element
 addElement(Qtree, Pos, Property) ->
-	send_add(Qtree, {element, Pos, Property}).
+    send_add(Qtree, {element, Pos, Property}).
 
-% mapFunction(Qtree, MapFun, Bound) ->
-% 	....
+mapFunction(Qtree, MapFun, Bound) ->
+	send_mapFun(Qtree, MapFun, Bound).
 
-% mapTreeFunction(Qtree, MapTreeFun) ->
-% 	....
+mapTreeFunction(Qtree, MapTreeFun) ->
+	send_mapTree(Qtree, MapTreeFun).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Communication functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Asynchronous communication
-info(Pid, Msg) ->
-	Pid ! Msg.
+info(Pid, Msg) -> Pid ! Msg.
 
 send_add(Pid, Element) ->
-	info(Pid, {addElement,Element}).
+    info(Pid, {addElement, Element}).
 
 send_mapTree(Pid, MapTreeFun) ->
-	info(Pid, {mapTreeFun, MapTreeFun}).
+    info(Pid, {mapTreeFun, MapTreeFun}).
 
 send_mapFun(Pid, MapFun, MapBound) ->
-	info(Pid, {mapFun, MapFun, MapBound}).
+    info(Pid, {mapFun, MapFun, MapBound}).
 
-send_stop(Pid) ->
-    info(Pid, stop).
+send_stop(Pid) -> info(Pid, stop).
 
 %% synchronous communication
 rpc(Pid, Request) ->
-	Pid ! {self(), Request},
-	receive
-	{Pid, Response} ->
-		Response
-	end.
+    Pid ! {self(), Request},
+    receive {Pid, Response} -> Response end.
 
-reply(From, Msg) ->
-    From ! {self(), Msg}.
+reply(From, Msg) -> From ! {self(), Msg}.
 
-reply_ok(From) ->
-    reply(From, ok).
+reply_ok(From) -> reply(From, ok).
 
-reply_ok(From, Msg) ->
-    reply(From, {ok, Msg}).
+reply_ok(From, Msg) -> reply(From, {ok, Msg}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Internal Implementation
@@ -82,67 +71,84 @@ reply_ok(From, Msg) ->
 
 %% Quadtree Coordinator
 quadtreeCoordinatorInit(Bound, Limit) ->
-	Pid = self(),
-	Leaf = spawn(fun() -> qtreeLeaf(Bound, Limit, Pid, []) end),
-	quadtreeCoordinator(Leaf, Bound).
+    Pid = self(),
+    Leaf = spawn(fun () -> qtreeLeaf(Bound, Limit, Pid, []) end),
+    quadtreeCoordinator(Leaf, Bound).
 
-quadtreeCoordinator (QtreeTop, Bound) ->
-	io:format("~p is coordinator for ~p with bound ~p~n", [self(), QtreeTop, Bound]),
- 	receive
- 		stop ->
- 			send_stop(QtreeTop),
- 			ok;
- 		{addElement, Element} ->
- 			case outOfBound(Bound, Element) of
- 				false -> send_add(QtreeTop, Element);
- 				true -> ok
- 			end,
- 			quadtreeCoordinator(QtreeTop, Bound)
- 	end.
-
+quadtreeCoordinator(QtreeTop, Bound) ->
+    receive
+    	stop -> send_stop(QtreeTop), ok;
+      	{addElement, Element} ->
+	  		case outOfBound(Bound, Element) of
+	    		false -> send_add(QtreeTop, Element);
+	    		true -> ok
+	  		end;
+	  	{mapFun, MapFun, MapBound} ->
+	  		case intersectBounds(MapBound, Bound) of
+	  			{X1,Y1,X2,Y2} -> send_mapFun(QtreeTop, MapFun, {X1, Y1, X2, Y2});
+	  			empty -> ok
+	  		end;
+	  	{mapTreeFun, TreeFun} -> send_mapTree(QtreeTop, TreeFun)
+    end,
+    quadtreeCoordinator(QtreeTop, Bound).
 
 %% Quadtree node
 qtreeNode(Bound, Limit, Parent, Children) ->
-	io:format("~p is node with parent ~p, bound ~p and children ~p~n", [self(), Parent, Bound, Children]),
-	receive
-		stop ->
-			lists:map(fun send_stop/1, Children),
-			ok;
-		{addElement, Element} ->
-			case outOfBound(Bound, Element) of
-				false -> lists:map(fun(C) -> send_add(C, Element) end, Children);
-				true -> ok
-			end,
-			qtreeNode(Bound, Limit, Parent, Children)
-	end.
+    receive
+    	stop -> [send_stop(C) || {_, C} <- Children];
+      	{addElement, Element} ->
+	  		case outOfBound(Bound, Element) of
+	    		false ->
+					Bounds = [{Quarter, createBound(Quarter, Bound)} || Quarter <- qTreeQuarters()],
+					[Q | _] = qTreeQuarters() -- [Q || {Q, B} <- Bounds, outOfBound(B, Element)],
+					{Q, Child} = lists:keyfind(Q, 1, Children),
+					send_add(Child, Element);
+	    		true -> send_add(Parent, Element)
+	  		end,
+	  		qtreeNode(Bound, Limit, Parent, Children);
+	  	{mapFun, MapFun, MapBound} -> [mapFunChild(C, MapFun, MapBound, Bound) || C <- Children];
+	  	{mapTreeFun, TreeFun} ->
+	  		[send_mapTree(C, TreeFun) || {_, C} <- Children],
+	  		qtreeNode(Bound, Limit, Parent, Children)
+    end.
 
+mapFunChild({Quarter, Child}, MapFun, MapBound, Bound) ->
+	case intersectBounds(MapBound, createBound(Quarter, Bound)) of
+		{X1, Y1, X2, Y2} -> send_mapFun(Child, MapFun, {X1, Y1, X2, Y2});
+		empty -> ok
+	end.
 
 %% Quadtree leaf
 qtreeLeaf(Bound, Limit, Parent, Data) ->
-	io:format("~p is leaf with parent ~p and bound ~p and data ~p~n", [self(), Parent, Bound, Data]),
-	receive
-		stop ->
-			ok;
-		{addElement, Element} ->
-			case {outOfBound(Bound, Element), length(Data) < Limit} of
-				{false, true} ->
-					qtreeLeaf(Bound, Limit, Parent, [Element | Data]);
-				{false, false} ->
-					NewBounds = lists:map(fun(Quarter) -> createBound(Quarter, Bound) end, qTreeQuarters()),
+    receive
+     	stop -> ok;
+     	{addElement, Element} ->
+	  		case {outOfBound(Bound, Element), length(Data) < Limit} of
+	    		{false, true} -> qtreeLeaf(Bound, Limit, Parent, [Element | Data]);
+	    		{false, false} ->
+					NewBounds = [{Quarter, createBound(Quarter, Bound)} || Quarter <- qTreeQuarters()],
 					NewParent = self(),
-					Children = [spawn(fun() -> qtreeLeaf(NewBound, Limit, NewParent, []) end) || NewBound <- NewBounds],
-					
+					Children = [{Q, spawn(fun () -> qtreeLeaf(NewBound, Limit, NewParent, []) end)} || {Q, NewBound} <- NewBounds],
 					NewData = [Element | Data],
-					DataList = [[E || E <- NewData, not outOfBound(B, E)] || B <- NewBounds],
+					DataList = [[E || E <- NewData, not outOfBound(B, E)] || {_, B} <- NewBounds],
 					ZippedList = lists:zip(Children, DataList),
-					lists:map(fun({C, Es}) -> lists:map(fun(E) -> send_add(C,E) end, Es) end, ZippedList),
+					[fun({{_, C}, Es}) -> [send_add(C, E) || E <- Es] end(V) || V <- ZippedList],
 					qtreeNode(Bound, Limit, Parent, Children);
-				{true, _} ->
-					qtreeLeaf(Bound, Limit, Parent, Data)
-			end
-	end.
-
-
+	    		{true, _} -> qtreeLeaf(Bound, Limit, Parent, Data)
+	  		end;
+	  	{mapFun, MapFun, MapBound} ->
+	  		case intersectBounds(Bound, MapBound) of
+	  			{X1, Y1, X2, Y2} ->
+	  				NewData = [case outOfBound({X1, Y1, X2, Y2}, E1) of false -> MapFun(E1); true -> E1 end || E1 <- Data],
+	  				OutOfBoundData = [E2 || E2 <- NewData, outOfBound(Bound, E2)],
+	  				[send_add(Parent, E3) || E3 <- OutOfBoundData],
+	  				qtreeLeaf(Bound, Limit, Parent, NewData -- OutOfBoundData);
+	  			empty -> ok
+	  		end;
+	  	{mapTreeFun, TreeFun} ->
+	  		[TreeFun(E) || E <- Data],
+	  		qtreeLeaf(Bound, Limit, Parent, Data)
+    end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Helper functions
@@ -162,45 +168,45 @@ qtreeLeaf(Bound, Limit, Parent, Data) ->
 % Y
 
 % List with quarters in the quadtree.
-qTreeQuarters() -> [nw,ne,sw,se].
+qTreeQuarters() -> [nw, ne, sw, se].
 
 % Returns the 1/4 of Bound which is defined by Quarter from the list above.
 createBound(_, universe) -> universe;
 createBound(_, empty) -> empty;
-createBound(Quarter,Bound) -> 
-	{X1, Y1, X2, Y2} = Bound,
-	Xmid = X1 + ((X2 - X1) / 2),
-	Ymid = Y1 + ((Y2 - Y1) / 2),
-	case Quarter of
-		nw -> {X1,   Y1,   Xmid, Ymid};
-		ne -> {Xmid, Y1,   X2,   Ymid};
-		sw -> {X1,   Ymid, Xmid, Y2  };
-		se -> {Xmid, Ymid, X2,   Y2  }
-	end.
+createBound(Quarter, Bound) ->
+    {X1, Y1, X2, Y2} = Bound,
+    Xmid = X1 + (X2 - X1) / 2,
+    Ymid = Y1 + (Y2 - Y1) / 2,
+    case Quarter of
+      nw -> {X1, Y1, Xmid, Ymid};
+      ne -> {Xmid, Y1, X2, Ymid};
+      sw -> {X1, Ymid, Xmid, Y2};
+      se -> {Xmid, Ymid, X2, Y2}
+    end.
 
 % Predicate that returns if an Element is outside Bound.
 outOfBound(universe, _) -> false;
 outOfBound(empty, _) -> true;
 outOfBound(Bound, Element) ->
-	{X1, Y1, X2, Y2} = Bound,
-	{element, Pos, _} = Element,
-	{X,Y} = Pos,
-	(X < X1) or (X >= X2) or (Y < Y1) or (Y >= Y2).
+    {X1, Y1, X2, Y2} = Bound,
+    {element, Pos, _} = Element,
+    {X, Y} = Pos,
+    (X < X1) or (X >= X2) or (Y < Y1) or (Y >= Y2).
 
 % Finds the intersection between Bound1 and Bound2.
-intersectBounds(universe, Bound)    -> Bound;
-intersectBounds(Bound,    universe) -> Bound;
-intersectBounds(empty,    _    )    -> empty;
-intersectBounds(_,        empty)    -> empty;
-intersectBounds(Bound,    Bound)    -> Bound; 
-intersectBounds(Bound1,   Bound2)   ->
-	{X1_1, Y1_1, X1_2, Y1_2} = Bound1,
-	{X2_1, Y2_1, X2_2, Y2_2} = Bound2,
-	X1 = lists:max([X1_1, X2_1]),
-	Y1 = lists:max([Y1_1, Y2_1]),
-	X2 = lists:min([X1_2, X2_2]),
-	Y2 = lists:min([Y1_2, Y2_2]),
-	case (X1 >= X2) or (Y1 >= Y2) of
-		true  -> empty;
-		false -> {X1, Y1, X2, Y2}
-	end.
+intersectBounds(universe, Bound) -> Bound;
+intersectBounds(Bound, universe) -> Bound;
+intersectBounds(empty, _) -> empty;
+intersectBounds(_, empty) -> empty;
+intersectBounds(Bound, Bound) -> Bound;
+intersectBounds(Bound1, Bound2) ->
+    {X1_1, Y1_1, X1_2, Y1_2} = Bound1,
+    {X2_1, Y2_1, X2_2, Y2_2} = Bound2,
+    X1 = lists:max([X1_1, X2_1]),
+    Y1 = lists:max([Y1_1, Y2_1]),
+    X2 = lists:min([X1_2, X2_2]),
+    Y2 = lists:min([Y1_2, Y2_2]),
+    case (X1 >= X2) or (Y1 >= Y2) of
+      true -> empty;
+      false -> {X1, Y1, X2, Y2}
+    end.
